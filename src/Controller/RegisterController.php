2f202c639a8b358b9e33e3d1040b441d370d0fec
NoriTaka68 <said@evhr.net>
2022-04-04 15:40:25 +0200

    Add authentification

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+
+
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user = $form->getData();
+            
+            $doctrine = $this->getDoctrine()->getManager();
+            $doctrine->persist($user);
+            $doctrine->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+            'form' => $form->createView()
+        ]);
+    }
+}
