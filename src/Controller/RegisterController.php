889700a39351e4aa59df25b612db6b817099aa92
NoriTaka68 <said@evhr.net>
2022-05-02 11:07:14 +0200

    modification HashPassword

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class RegisterController extends AbstractController
 {
@@ -24,7 +26,7 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -35,7 +37,7 @@ class RegisterController extends AbstractController
 
             $user = $form->getData();
 
-            $password = $encoder->encodePassword($user, $user->getPassword());
+            $password = $encoder->hashPassword($user, $user->getPassword());
 
             $user->setPassword($password);
 
