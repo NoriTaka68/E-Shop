fe6161ecd5a11d587c2ea6654a1bc4052b3764e2
NoriTaka68 <said@evhr.net>
2022-06-11 11:25:45 +0200

    Formulaire pour changer sont Mot de Passe

--- src/Controller/AccountPassWordController.php
+++ src/Controller/AccountPassWordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,11 @@ class AccountPassWordController extends AbstractController
     #[Route('/compte/password', name: 'account_password')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
         return $this->render('account/password.html.twig', [
             'controller_name' => 'AccountPassWordController',
+            'form' =>$form->createView(),
         ]);
     }
 }
