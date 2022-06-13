46adcbb0bc55239796243fa84f90bd4c036cb9e8
NoriTaka68 <said@evhr.net>
2022-06-13 14:06:55 +0200

    formulaire de changement de MPD

--- src/Controller/AccountPassWordController.php
+++ src/Controller/AccountPassWordController.php
@@ -3,17 +3,48 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountPassWordController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte/password', name: 'account_password')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            
+
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+                
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+            }
+        }
+
         return $this->render('account/password.html.twig', [
             'controller_name' => 'AccountPassWordController',
             'form' =>$form->createView(),
