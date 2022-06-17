baaf85125be71cc90ba950c79f2acf9a9312027d
NoriTaka68 <said@evhr.net>
2022-06-17 14:59:04 +0200

    Dashboard

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -21,11 +23,13 @@ class ProductCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
             ImageField::new('illustration')
                 ->setUploadDir('public/ImagesProduits'),
             TextField::new('subtitle'),
             TextareaField::new('description'),
-            MoneyField::new('price')->setCurrency('EUR')
+            MoneyField::new('price')->setCurrency('EUR'),
+            AssociationField::new('category')
         ];
     }
     
