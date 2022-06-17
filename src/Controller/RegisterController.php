8aca566a020bf5e10c8fa24f587b24a377442d7e
NoriTaka68 <said@evhr.net>
2022-06-17 17:31:46 +0200

    suite Dashboard

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -48,6 +48,7 @@ class RegisterController extends AbstractController
             $this->entityManager->flush();
         }
 
+
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
             'form' => $form->createView()
